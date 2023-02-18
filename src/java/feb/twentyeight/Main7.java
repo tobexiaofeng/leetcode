004ce0a494372072b6e494e8b26feff2f584c77a
chenhuafeng <huamike1@gmail.com>
2023-02-18 18:51:36 +0800

    leetcode7 first commit

---
+++ src/java/feb/twentyeight/Main7.java
@@ -0,0 +1,23 @@
+package src.java.feb.twentyeight;
+
+public class Main7 {
+    public int reverse(int x) {
+        boolean ltZero = x < 0;
+        StringBuilder num = new StringBuilder(String.valueOf(Math.abs(x)));
+        num = num.reverse();
+        int index = 0;
+        while (index < num.length() && num.charAt(index) == '0') {
+            index++;
+        }
+        if(index == num.length()){
+            return 0;
+        }
+        int res = 0;
+        try{
+            res = Integer.parseInt(num.substring(index, num.length()));
+        }catch (Exception e){
+
+        }
+        return ltZero ? res * -1 : res;
+    }
+}
